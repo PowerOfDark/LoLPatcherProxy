928870956f36d1f8acf8c0ecdd7b5f246a955b7e
PowerOfDark <przem2003@o2.pl>
2015-09-08 16:25:27 +0200

    Initial commit

---
+++ HttpProxy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace HttpProxy
+{
+    class Program
+    {
+        public static byte[] localhost;
+        public static byte[] riothost;
+        public static int diff;
+        static void Main(string[] args)
+        {
+            localhost = Encoding.UTF8.GetBytes("Host: 127.0.0.1");//http header of our tweaked patcher
+            riothost = Encoding.UTF8.GetBytes("Host: l3cdn.riotgames.com");//http header needed
+            diff = Math.Abs(riothost.Length - localhost.Length);
+
+            IPAddress ip = Dns.GetHostAddresses("l3cdn.riotgames.com")[0];
+            Console.WriteLine("You can now start the clean patcher");
+            SimpleProxy p = new SimpleProxy(new IPEndPoint(IPAddress.Loopback, 80), "l3cdn.riotgames.com", 80);
+
+            Console.WriteLine("The end?");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HttpProxy/SimpleProxy.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpProxy
+{
+    public class SimpleProxy
+    {
+        public Socket MainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public List<Tuple<Socket, Socket>> Sockets = new List<Tuple<Socket, Socket>>();
+        public Task ClientTask, ServerTask;
+
+
+        private static byte[] GetFakeResponse(string body)
+        {
+            string temp = "HTTP/1.1 200 OK\r\nServer: Apache\r\nAccept-Ranges: bytes\r\nContent-Type: text/plain\r\nConnection: keep-alive\r\nPragma: no-cache\r\nCache-Control: max-age=0, no-cache, no-store\r\n";
+            int bytes = body.Length;
+            byte[] buffer;
+            temp += "Content-Length: " + bytes + "\r\n\r\n";
+            temp += body;
+            buffer = Encoding.UTF8.GetBytes(temp);
+            return buffer;
+
+        }
+
+        private static byte[] Replace(byte[] input, byte[] pattern, byte[] replacement)
+        {
+            if (pattern.Length == 0)
+            {
+                return input;
+            }
+
+            List<byte> result = new List<byte>();
+
+            int i;
+
+            for (i = 0; i <= input.Length - pattern.Length; i++)
+            {
+                bool foundMatch = true;
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (input[i + j] != pattern[j])
+                    {
+                        foundMatch = false;
+                        break;
+                    }
+                }
+
+                if (foundMatch)
+                {
+                    result.AddRange(replacement);
+                    i += pattern.Length;
+                    break;//found one match, I'M DONE
+                }
+                else
+                {
+                    result.Add(input[i]);
+                }
+            }
+
+            for (; i < input.Length; i++)
+            {
+                result.Add(input[i]);
+            }
+
+            return result.ToArray();
+        }
+
+        private void ClientToServerTask(int id)
+        {
+            bool error = false;
+            byte[] buffer = new byte[8192];
+            int bytesRead = 0;
+            string data, type = "", path = "", body;
+            bool requestCompleted = false;
+
+            Socket clientSocket = Sockets[id].Item1;
+            Socket serverSocket = Sockets[id].Item2;
+
+            while (!error)
+            {
+                try
+                {
+                    requestCompleted = false;
+                    buffer = new byte[8192];
+                    bytesRead = clientSocket.Receive(buffer, 8192, SocketFlags.None);
+                    if (bytesRead == 0)
+                        throw new Exception();
+                    buffer = Replace(buffer, Program.localhost, Program.riothost);
+                    bytesRead += Program.diff;
+                    data = Encoding.UTF8.GetString(buffer, 0, 4);
+                    if (data == "GET ")
+                    {
+                        type = "GET";
+                        data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        path = data.Split(' ')[1];
+                        if (File.Exists("httpd" + path))
+                        {
+                            type = "OVERRIDEN GET";
+                            using (StreamReader sr = new StreamReader("httpd" + path))
+                            {
+                                body = sr.ReadToEnd();
+                            }
+                            body = body.Replace("\r\n", "\n");
+                            body = body.Replace("\n", "\r\n");
+                            buffer = GetFakeResponse(body);
+                            bytesRead = buffer.Length;
+                            clientSocket.BeginSend(buffer, 0, bytesRead, SocketFlags.None, null, null);//send back the fake request to client,
+                            requestCompleted = true;//without sending anything to server
+                        }
+
+                    }
+
+                    if (!requestCompleted)
+                        serverSocket.BeginSend(buffer, 0, bytesRead, SocketFlags.None, null, null);
+                    Console.WriteLine("[{0}] {1}", type, path);
+                }
+                catch { error = true; }
+            }
+            try
+            {
+                clientSocket.Close();
+            }
+            catch { }
+
+            try
+            {
+                serverSocket.Close();
+            }
+            catch { }
+            Console.WriteLine("Client task #{0} ended", id);
+        }
+
+        private void ServerToClientTask(int id)
+        {
+            bool error = false;
+            byte[] buffer = new byte[8192];
+            int bytesRead = 0;
+            Socket clientSocket = Sockets[id].Item1;
+            Socket serverSocket = Sockets[id].Item2;
+
+            while (!error)
+            {
+                try
+                {
+                    buffer = new byte[8192];
+                    bytesRead = serverSocket.Receive(buffer, 8192, SocketFlags.None);
+                    if (bytesRead == 0)
+                        throw new Exception();
+                    if (clientSocket != null)
+                        clientSocket.BeginSend(buffer, 0, bytesRead, SocketFlags.None, null, null);
+                }
+                catch { error = true; }
+            }
+            try
+            {
+                serverSocket.Close();
+            }
+            catch { }
+
+            try
+            {
+                clientSocket.Close();
+            }
+            catch { }
+
+            Console.WriteLine("Server task #{0} ended", id);
+        }
+
+
+        public SimpleProxy(EndPoint local, string remote, int remotePort)
+        {
+            MainSocket.Bind(local);
+            MainSocket.Listen(10);
+            while (true)
+            {
+                try
+                {
+                    int id = Sockets.Count;
+                    Socket ClientSocket = MainSocket.Accept();
+
+                    Console.WriteLine("Connection from " + ClientSocket.RemoteEndPoint.ToString());
+                    Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    ServerSocket.Connect(remote, remotePort);
+                    Console.WriteLine("Connection to server established.");
+                    Sockets.Add(new Tuple<Socket, Socket>(ClientSocket, ServerSocket));
+                    Task.Factory.StartNew(() => { ServerToClientTask(id); });
+                    Task.Factory.StartNew(() => { ClientToServerTask(id); });
+
+                }
+                catch { Console.WriteLine("ERROR while setting up"); }
+            }
+        }
+    }
+}
